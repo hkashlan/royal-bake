affb799ce92982fef78511f9e4325b188d283012
dell omar alzamel <alzimelomar@gmail.com>
2025-01-27 00:40:14 +0300

    ---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HaderComponent } from "./shared/hader/hader.component";
 import { AboutComponent } from "./shared/about/about.component";
 import { SadbarComponent } from "./page/sadbar/sadbar.component";
+import { About1Component } from './page/about-1/about-1.component';
+import { RivelComponent } from './page/rivel/rivel.component';
+import { ChefComponent } from "./page/chef/chef.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HaderComponent, AboutComponent, SadbarComponent],
+  imports: [RouterOutlet, HaderComponent, AboutComponent, SadbarComponent, About1Component, RivelComponent, ChefComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/page/about-1/about-1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-1',
+  imports: [],
+  templateUrl: './about-1.component.html',
+  styleUrl: './about-1.component.css'
+})
+export class About1Component {
+
+}
