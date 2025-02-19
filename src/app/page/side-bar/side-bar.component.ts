dc463ac6c3e4149ea0e65c9a0edb3be720714032
hadi <hadi@MacBookPro.fritz.box>
2025-02-19 16:59:59 +0100

    fix autoplay

--- src/app/page/side-bar/side-bar.component.ts
+++ src/app/page/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { afterNextRender, Component, ViewChild } from '@angular/core';
 
 interface Slide {
   img: string;
@@ -9,6 +9,8 @@ interface Slide {
 
 
 import Swiper from 'swiper';
+import { Autoplay, Navigation } from 'swiper/modules';
+Swiper.use([Autoplay, Navigation]);
 
 
 @Component({
@@ -17,13 +19,37 @@ import Swiper from 'swiper';
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss',
 })
-export class SideBarComponent implements OnInit{
+export class SideBarComponent {
   @ViewChild('swiperContainer', { static: true }) swiperRef!: Swiper;
 
   swiper!: Swiper
 
 
   slides: Slide[] = [
+    {
+      img: '/special.jpg',
+      title: 'PASTA',
+      info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est.',
+      price: '$11.30',
+    },
+    {
+      img: '/special.jpg',
+      title: 'PASTA',
+      info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est.',
+      price: '$12.30',
+    },
+    {
+      img: '/special.jpg',
+      title: 'PASTA',
+      info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est.',
+      price: '$13.30',
+    },
+    {
+      img: '/special.jpg',
+      title: 'PASTA',
+      info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est.',
+      price: '$11.30',
+    },
     {
       img: '/special.jpg',
       title: 'PASTA',
@@ -56,20 +82,35 @@ export class SideBarComponent implements OnInit{
     },
   ];
 
+  constructor() {
+    afterNextRender(() => {
+      this.swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+          delay: 2000, // Delay between transitions (in ms)
+          disableOnInteraction: false, // Continue autoplay after user interaction
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        },
+
+      });
+      // setTimeout(() => {
+
+      //   this.swiper.autoplay.start();
+      // }, 100);
+    });
+  }
+
   plusamg() {}
 
 
-  ngOnInit() {
-    this.swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
 
-    console.log(this.swiper);
-  }
 }
