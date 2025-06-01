ee8b837fe084b08aa8d6649e7382647b73adc19b
hkashlan <hkashlan@users.noreply.github.com>
2025-06-01 14:43:37 +0000

    deploy: c50244454ff0a0934f5cec841cf3f3afcf77a1c3

---
+++ server/assets-chunks/index_server_html.mjs
@@ -0,0 +1,54 @@
+export default `<!DOCTYPE html>
+<html lang="en">
+
+   <head><base href="/">
+      <meta charset="UTF-8">
+
+      <title>Restfood - HTML Template Restaurant</title>
+
+      <meta name="description" content="">
+      <meta name="keywords" content="">
+
+      <!-- Always force latest IE rendering engine -->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+      <!-- Mobile Specific Meta -->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+      <!-- Custom style CSS -->
+      <link rel="stylesheet" href="css/style.css">
+
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="css/responsive.css">
+   <link rel="stylesheet" href="styles-5INURTSO.css"></head>
+   <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
+</script>
+      <app-root></app-root>
+      <!-- ====== Preloader ====== -->
+      <div id="preload">
+         <img src="images/preloader.png" alt="">
+      </div>
+      <!-- ======End Preloader ====== -->
+
+      <!-- ====== Scroll Top ====== -->
+      <a class="btn" id="scroll-top" href="#"><i class="fa-solid fa-arrow-up-long"></i></a>
+      <!-- ====== End Scroll Top ====== -->
+
+      <!-- ====== jQuery ====== -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+      <!-- ====== Carousel ====== -->
+      <script src="js/owl.carousel.min.js"></script>
+
+      <!-- ====== Magnific popup ====== -->
+      <script src="js/jquery.magnific-popup.min.js"></script>
+
+      <!-- ====== Custom js ======  -->
+      <script src="js/script.js"></script>
+
+   <script src="main-A7MBWUJW.js" type="module"></script></body>
+</html>
+`;
